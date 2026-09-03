9e3b59e691ff38aa2c75a73b6e869e6aec891deb
GiuGiu <giulianodenardi@gmail.com>
2026-09-03 01:42:47 -0300

    Update config.h

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 #define CONFIG_H
 
 // Project Metadata
-#define AVRMON_VERSION "v0.1"
+#define AVRMON_VERSION "v0.2"
 
 // System Configuration
-#define AVRMON_BAUD_RATE 9600
+#define AVRMON_BAUD_RATE 38400
 #define AVRMON_BUFFER_SIZE 64
 #define AVRMON_MAX_DUMP_LEN 0x0100
 
